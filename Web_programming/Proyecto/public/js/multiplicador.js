4f9626b59f5d3f3e95a627009c94eb306cf633a2
ulmo369 <a01366802@itesm.mx>
2022-06-21 00:29:28 +0200

    MultiAgents and app

--- Web_programming/Proyecto/public/js/multiplicador.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("multiplicador").oninput=()=>{
-    document.getElementById("colchon").value=document.getElementById("multiplicador").value*document.getElementById("tiempo3").value;
-}
--- Web_programming/Proyecto/util/is-miembro.js
+++
@@ -1,6 +0,0 @@
-module.exports = (request, response, next) => {
-    if (request.session.rol != 78000) {
-        return response.redirect('/');
-    }
-    next();
-}
